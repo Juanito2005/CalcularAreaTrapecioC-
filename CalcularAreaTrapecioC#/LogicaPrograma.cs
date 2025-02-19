4b502f5ed4f4b8a9c373d949155d2831ce9a0dea
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2025-02-19 13:50:50 +0100

    project complete

---
+++ CalcularAreaTrapecioC#/LogicaPrograma.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularAreaTrapecioC_
+{
+    internal class LogicaPrograma
+    {
+        internal float CalcularAreaTrapecio(float baseMayor, float baseMenor, float altura)
+        {
+            float area = ((baseMayor + baseMenor) * altura) / 2;
+            return area;
+        }
+
+        internal void MostrarResultado(float resultado)
+        {
+            Console.WriteLine("Y el area del trapecio en base a tus datos introducidos es: ");
+            Console.WriteLine(resultado);
+        }
+
+        internal float ValidacionEntrada(string texto)
+        {
+            float numero;
+            while (true)
+            {
+                Console.WriteLine(texto);
+                string entradaString = Console.ReadLine();
+                if (float.TryParse(entradaString, out numero) && numero > 0)
+                {
+                    return numero;
+                }
+                else if (float.TryParse(entradaString, out numero) && numero <= 0)
+                {
+                    Console.WriteLine("El número debe ser positivo. Ingresa otro valor :)");
+                }
+                else
+                {
+                    Console.WriteLine("Las letras y simbolos no son permitidos!!!");
+                }
+            }
+        }
+    }
+}
---
+++ CalcularAreaTrapecioC#/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CalcularAreaTrapecioC_
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, bienvenido al programa para calcular el area de un trapecio :)");
+            LogicaPrograma logica = new LogicaPrograma();
+
+            float baseMa = logica.ValidacionEntrada("Ingresa la base mayor del trapecio:");
+            float baseMe = logica.ValidacionEntrada("Ingresa la base menor del trapecio:");
+            float altura = logica.ValidacionEntrada("Por ultimo ingresa la altura del trapecio:");
+
+            float resultado = logica.CalcularAreaTrapecio(baseMa, baseMe, altura);
+            logica.MostrarResultado(resultado);
+
+        }
+    }
+}
